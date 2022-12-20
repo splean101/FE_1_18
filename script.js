fe256156a6624d0a5dcf8cabcdaebda2ffb6d495
splean101 <101splean@gmail.com>
2022-12-20 14:44:39 +0200

    4 done

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ let secondMonthSalary = Number(prompt('Enter second month salary', '0'));
 alert(`Your salary for 2 month is ${firstMonthSalary + secondMonthSalary}`);
 
 //2.
-alert(`You have extra bonus! Your total salary was: ${firstMonthSalary + secondMonthSalary}$
-It's  increase for 1$ and now it is ${firstMonthSalary + secondMonthSalary + 1}$`);
+alert(`You have extra bonus! Your total salary was: ${
+  firstMonthSalary + secondMonthSalary
+}$
+It's  increase for 1$ and now it is ${
+  firstMonthSalary + secondMonthSalary + 1
+}$`);
 
 //3.
 const MinSalaryLevel = 2000;
@@ -17,4 +21,4 @@ alert(
 );
 
 //4.
-
+alert(firstMonthSalary + secondMonthSalary + 1 >= MinSalaryLevel && "I'm ready to work" || "I'm out");
