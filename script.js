6aa62fd8eaed64cc99b338e6c21f65c69fd126ad
splean101 <101splean@gmail.com>
2022-12-20 14:38:01 +0200

    some changes

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 //1.
 
-// let firstMonthSalary = Number(prompt('Enter first month salary', '0'));
-// let secondMonthSalary = Number(prompt('Enter second month salary', '0'));
-// alert(`Your salary for 2 month is ${firstMonthSalary + secondMonthSalary}`);
+let firstMonthSalary = Number(prompt('Enter first month salary', '0'));
+let secondMonthSalary = Number(prompt('Enter second month salary', '0'));
+alert(`Your salary for 2 month is ${firstMonthSalary + secondMonthSalary}`);
 
 //2.
-// let firstMonthSalary1 = Number(prompt('Enter first month salary', '0'));
-// let secondMonthSalary1 = Number(prompt('Enter second month salary', '0'));
-// alert(`You have extra bonus! Your total salary was: ${firstMonthSalary1 + secondMonthSalary1}$
-// It's  increase for 1$ and now it is ${firstMonthSalary1 + secondMonthSalary1 + 1}$`);
+alert(`You have extra bonus! Your total salary was: ${firstMonthSalary + secondMonthSalary}$
+It's  increase for 1$ and now it is ${firstMonthSalary + secondMonthSalary + 1}$`);
 
 //3.
 const MinSalaryLevel = 2000;
-let firstMonthSalary2 = Number(prompt('Enter first month salary', '0'));
-let secondMonthSalary2 = Number(prompt('Enter second month salary', '0'));
 alert(
   `Will you work next? ${
-    firstMonthSalary2 + secondMonthSalary2 + 1 >= MinSalaryLevel
+    firstMonthSalary + secondMonthSalary + 1 >= MinSalaryLevel
   }`
 );
+
+//4.
+
